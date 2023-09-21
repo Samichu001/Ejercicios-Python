e0fc6c8c25a4a007f56a77c432bacff448022742
Samichu001 <zudaire.alsam22@zaragoza.salesianos.edu>
2023-09-21 13:52:04 +0200

    Ejercicio 3 añadido

---
+++ Ejercicio 3.py
@@ -0,0 +1,15 @@
+import random
+
+aleatorio = random.randint(0, 20)
+numero = 0
+
+while numero != aleatorio:
+    numero = int(input("Intenta adivinar que numero del 0 al 20 ha salido"))
+    if numero < aleatorio:
+        print("El numero es mayor")
+    elif numero > aleatorio:
+        print("El numero es mas pequeño")
+    else:
+        print("Enhorabuena!!!")
+        break
+ 
