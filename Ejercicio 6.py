7ded865cfe58a83d8fa5813234982a57176ff49a
Samichu001 <zudaire.alsam22@zaragoza.salesianos.edu>
2023-09-26 18:09:36 +0200

    Ejercicio 6 acabado

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -1,2 +1,12 @@
 cadena = str(input("Introduce una cadena de caracteres"))
 
+if len(cadena) >= 2:
+    primero = cadena[0]
+    ultimo = cadena[-1]
+
+    cadena_intercambiada = ultimo + cadena[1:-1] + primero
+
+    print(cadena_intercambiada)
+
+else:
+    print("La cadena de caracteres debe tener al menos dos caracteres")
