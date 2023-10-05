f588bdd43adb387efe63864edaebf14eaa955369
Samichu001 <zudaire.alsam22@zaragoza.salesianos.edu>
2023-10-05 12:46:01 +0200

    Ej 11 acabado

--- Ejercicio 11.py
+++ Ejercicio 11.py
@@ -1,9 +1,10 @@
-salarios = [
-    [700, 900, 1300],
-    [1000, 950, 1080],
-    [1300, 930, 1200]
-]
+salarios = [[700, 900, 1300], [1000, 950, 1080], [1300, 930, 1200]]
 
-empleados = {"Javier Maria", "Anotnio Muñoz", "Isabel Allende"}
+empleados = ["Javier Maria", "Anotnio Muñoz", "Isabel Allende"]
 
-print(empleados)
+for x in range(len(empleados)):
+    nombre_empleado = empleados[x]
+    salario_separado = salarios[x]
+    salario_total = sum(salario_separado)
+
+    print(nombre_empleado, " + ", salario_separado, " = ", salario_total)
